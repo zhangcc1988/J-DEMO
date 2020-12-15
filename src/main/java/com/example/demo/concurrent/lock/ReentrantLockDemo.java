3dad43109eb388f6c263993317050afe672af966
章超 <1277188108@qq.com>
2020-12-15 14:32:01 +0800

    m

--- src/main/java/com/example/demo/concurrent/lock/ReentrantLockDemo.java
+++ src/main/java/com/example/demo/concurrent/lock/ReentrantLockDemo.java
@@ -1,14 +1,12 @@
 package com.example.demo.concurrent.lock;
 
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class ReentrantLockDemo {
 
     public static void main(String[] args) {
 
-        ReentrantLock reentrantLock = new ReentrantLock(false);
-        AtomicInteger atomicInteger = new AtomicInteger(10);
-        ThreadLocal<Object> objectThreadLocal = new ThreadLocal<>();
+//        ReentrantLock reentrantLock = new ReentrantLock(false);
+//        AtomicInteger atomicInteger = new AtomicInteger(10);
+//        ThreadLocal<Object> objectThreadLocal = new ThreadLocal<>();
+
     }
 }
---
+++ src/main/java/com/example/demo/concurrent/threadPool/ScheduledDemo.java
@@ -0,0 +1,35 @@
+package com.example.demo.concurrent.threadPool;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+/**
+ * created by zc  2020/11/19 10:34
+ */
+public class ScheduledDemo {
+
+    static ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors(),
+            r -> {
+                Thread thread = new Thread(r);
+                thread.setDaemon(true);
+                return thread;
+            });
+
+    public static void main(String[] args) throws InterruptedException {
+//        scheduledExecutorService.scheduleAtFixedRate(() -> {
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            System.out.println(Thread.currentThread().getName());
+//        },0,5, TimeUnit.MILLISECONDS);
+
+        System.out.println(scheduledExecutorService.isShutdown());
+        scheduledExecutorService.shutdown();
+        System.out.println(scheduledExecutorService.isShutdown());
+
+    }
+
+
+}
